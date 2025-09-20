e62c8fbcec081312e5102ba04363a714f2d76fc4
Benjakusa <benjakusa@gmail.com>
2025-09-20 14:56:45 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names) {
+  const messagesArray = [];
+  for (let i = 0; i < names.length; i++) {
+    messagesArray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+  return messagesArray;
+}
+
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"])); 
+
+
+  //countdown
+   function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+countDown(10);
